4a6210d8ccde527559ab7ee383095289f737454f
Hiran Madhushanka <hiranmadhushanka123@gmail.com>
2019-02-21 13:22:16 +0530

    firstcommit

---
+++ src/multiplayerscroecard/MultiPlayerScroeCard.java
@@ -0,0 +1,12 @@
+
+package multiplayerscroecard;
+
+
+public class MultiPlayerScroeCard {
+
+    
+    public static void main(String[] args) {
+       ScoreCard s = new ScoreCard();
+       s.setVisible(true);
+    }
+}
